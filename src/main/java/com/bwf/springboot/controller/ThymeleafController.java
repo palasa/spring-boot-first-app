c9476c1c0b81ac7b4a227312852fdada4a43703b
parasa <parasa001@hotmail.com>
2018-08-24 16:54:37 +0800

    thymeleaf test2

--- src/main/java/com/bwf/springboot/controller/ThymeleafController.java
+++ src/main/java/com/bwf/springboot/controller/ThymeleafController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("th")
@@ -31,13 +33,33 @@ public class ThymeleafController {
     @RequestMapping("test")
     public String test(ModelMap modelMap) {
         User user = new User();
-        user.setUsername("张三");
+        user.setUsername("Anderson");
         user.setPassword("123456");
         user.setAge(18);
         user.setDesc("<span style='color:red'>hello</span>");
         user.setBirthday(new Date());
 
         modelMap.addAttribute("user", user);
+
+        User user1 = new User();
+        user1.setUsername("Jack");
+        user1.setAge(21);
+
+        User user2 = new User();
+        user2.setUsername("Wang");
+        user2.setAge(53);
+
+        List<User> userList = new ArrayList<User>();
+        userList.add(user);
+        userList.add(user1);
+        userList.add(user2);
+
+        modelMap.addAttribute("userList", userList);
+
+
+
+
+
         return "test";
     }
 
